899e3b6f14fffc593753957304c2653a69583c61
Navid Rahman <navidrahman92@gmail.com>
2022-07-23 19:04:10 +0600

    Countdown Activity Completed.

--- app/src/main/java/com/navid/clock/Countdown.java
+++ app/src/main/java/com/navid/clock/Countdown.java
@@ -4,16 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Countdown extends AppCompatActivity {
     private Button button;
+    private static final long START_TIME_IN_MILLIS = 60000;
+
+    private TextView TV;
+    private Button BT_startButton;
+    private Button BT_pauseButton;
+    private Button BT_resetButton;
+
+    private CountDownTimer countDownTimer;
+    private boolean TimeRunning;
+    private long TimeLeftInMillis = START_TIME_IN_MILLIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countdown);
+
+        TV = findViewById(R.id.countdown_textview);
+        BT_startButton = findViewById(R.id.startButton);
+        BT_pauseButton = findViewById(R.id.pauseButton);
+        BT_resetButton = findViewById(R.id.resetButton);
+
+        BT_startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TimeRunning == false){
+                    startTimer();
+                }
+            }
+        });
+
+        BT_pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TimeRunning == true){
+                    pauseTimer();
+                }
+            }
+        });
+
+        BT_resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+
 
         button = (Button) findViewById(R.id.backCountdown);
         button.setOnClickListener(new View.OnClickListener(){
